a31889bb2078fab9ffb072255c7546b3e2acf43b
matejsarlija <matejsarlija1@gmail.com>
2023-01-29 19:17:59 +0100

    middleware; addHttpContextAccessor

--- Program.cs
+++ Program.cs
@@ -1,25 +1,63 @@
+using Microsoft.AspNetCore.DataProtection;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
+// request / resp go through Services
 builder.Services.AddDataProtection();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<AuthService>();
 
 var app = builder.Build();
 
-
-app.MapGet("/user", (HttpContext ctx) =>
+// then go through middleware
+app.Use((ctx, next) =>
 {
+    var idp = ctx.RequestServices.GetRequiredService<IDataProtectionProvider>();
+    var protector = idp.CreateProtector("auth-cookie");
+
     var authCookie = ctx.Request.Headers.Cookie.FirstOrDefault(x => x.StartsWith("auth="));
-    var payload = authCookie.Split("=").Last();
+    var protectedPayload = authCookie.Split("=").Last();
+    var payload = protector.Unprotect(protectedPayload);
     var parts = payload.Split(":");
     var key = parts[0];
     var value = parts[1];
-    return key + value;
+
+
+    return next();
+
+});
+
+app.MapGet("/user", (HttpContext ctx, IDataProtectionProvider idp) =>
+{
+
+
+    return value;
 
 });
 
 app.MapGet("/login", (HttpContext ctx) =>
 {
-    ctx.Response.Headers["set-cookie"] = "auth=user:me";
-    return "ok";
+    return ctx.User;
 });
 
 app.Run();
+
+public class AuthService
+{
+    private readonly IDataProtectionProvider _idp;
+    private readonly IHttpContextAccessor _accessor;
+
+    public AuthService(IDataProtectionProvider idp, IHttpContextAccessor accessor)
+    {
+        _idp = idp;
+        _accessor = accessor;
+    }
+
+    public void SignIn()
+    {
+        var protector = _idp.CreateProtector("auth-cookie");
+
+        _accessor.HttpContext.Response.Headers["set-cookie"] = $"auth={protector.Protect("usr:me")}";
+    }
+}
